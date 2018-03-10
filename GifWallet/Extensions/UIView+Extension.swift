0daf5389a84e685b2adfadeea8675043362db7c5
Pierluigi Cifani <pcifani@blurredsoftware.com>
2018-03-10 18:58:35 +0100

    First iteration using ScrollView

--- GifWallet/Extensions/UIView+Extension.swift
+++ GifWallet/Extensions/UIView+Extension.swift
@@ -11,5 +11,15 @@ extension UIView {
         subview.translatesAutoresizingMaskIntoConstraints = false
         self.addSubview(subview)
     }
-
+    
+    public func pinToSuperview(withEdges edges: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)) {
+        guard let superView = superview else { return }
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: edges.left),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -edges.right),
+            topAnchor.constraint(equalTo: superView.topAnchor, constant: edges.top),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -edges.bottom)
+            ])
+    }
 }
