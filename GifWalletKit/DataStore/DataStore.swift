6c9319d3c4bf335395c344a5511ef404f9315bc4
Pierluigi Cifani <pcifani@blurredsoftware.com>
2018-03-28 14:33:13 +0200

    Create and fetch GIFs given an ID

--- GifWalletKit/DataStore/DataStore.swift
+++ GifWalletKit/DataStore/DataStore.swift
@@ -8,11 +8,6 @@ import Deferred
 
 public class DataStore {
 
-    public enum Kind {
-        case sqlite
-        case memory
-    }
-
     private let persistentStore: NSPersistentContainer
     public var storeIsReady: Bool = false
 
@@ -45,8 +40,57 @@ public class DataStore {
         }
         return Task(deferred)
     }
+
+    //MARK: GIF Creation
+    func createGIF(giphyID: String, title: String, subtitle: String, url: URL, tags: Set<String>) -> Task<()> {
+        let deferred = Deferred<TaskResult<()>>()
+        guard self.storeIsReady else {
+            deferred.fill(with: TaskResult<()>(failure: DataStore.Error.dataStoreNotInitialized))
+            return Task(deferred)
+        }
+
+        self.persistentStore.performBackgroundTask { (moc) in
+            let managedGIF = ManagedGIF(entity: ManagedGIF.entity(), insertInto: moc)
+            managedGIF.title = title
+            managedGIF.subtitle = subtitle
+            managedGIF.remoteURL = url.absoluteString
+            managedGIF.giphyID = giphyID
+            managedGIF.creationDate = Date()
+
+            do {
+                try moc.save()
+                deferred.fill(with: .init(success: ()))
+            } catch let error {
+                deferred.fill(with: .init(failure: error))
+            }
+        }
+        return Task(deferred)
+    }
+
+    func fetchGIF(id: String) throws -> ManagedGIF? {
+        guard self.storeIsReady else {
+            throw DataStore.Error.dataStoreNotInitialized
+        }
+
+        let fetchRequest: NSFetchRequest<ManagedGIF> = ManagedGIF.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "giphyID == %@", id)
+        let managedGIFs = try self.persistentStore.viewContext.fetch(fetchRequest)
+        return managedGIFs.first
+    }
 }
 
+
+extension DataStore {
+
+    public enum Kind {
+        case sqlite
+        case memory
+    }
+
+    public enum Error: Swift.Error {
+        case dataStoreNotInitialized
+    }
+}
 extension DataStore.Kind {
     fileprivate var coreDataRepresentation: String {
         switch self {
--- GifWalletKitTests/DataStoreTests.swift
+++ GifWalletKitTests/DataStoreTests.swift
@@ -13,13 +13,32 @@ class DataStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         dataStore = DataStore(kind: .memory)
-    }
-
-    func testInit() throws {
         let task = dataStore.loadAndMigrateIfNeeded()
-        _ = try self.waitForTask(task)
+        _ = try? waitForTask(task)
         XCTAssert(dataStore.storeIsReady)
     }
 
+    func testCreateAndFetchGIF() throws {
+        let id = "007"
+        let createTask = dataStore.createGIF(
+            giphyID: "007",
+            title: "James Bond",
+            subtitle: "GoldenEye",
+            url: URL(string: "google.com/007")!,
+            tags: ["007"]
+        )
+        _ = try waitForTask(createTask)
+        
+        guard let managedGIF = try dataStore.fetchGIF(id: id) else {
+            throw Error.objectUnwrappedFailed
+        }
+        XCTAssert(managedGIF.giphyID == id)
+        XCTAssert(managedGIF.title == "James Bond")
+        XCTAssert(managedGIF.creationDate != nil)
+    }
+
+    enum Error: Swift.Error {
+        case objectUnwrappedFailed
+    }
 }
 
