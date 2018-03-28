4a91dbe9a497b61e18d7f64bea539ab965865631
Pierluigi Cifani <pcifani@blurredsoftware.com>
2018-03-28 13:35:09 +0200

    Create DataStore, Model and Tests

---
+++ GifWalletKit/DataStore/DataStore.swift
@@ -0,0 +1,26 @@
+//
+//  Created by Pierluigi Cifani on 28/03/2018.
+//  Copyright © 2018 Code Crafters. All rights reserved.
+//
+
+import CoreData
+
+class DataStore {
+
+    let persistentStore: NSPersistentContainer
+
+    init() {
+
+        guard
+            let path = Bundle(for: DataStore.self).path(forResource: "Model", ofType: "momd"),
+            let model = NSManagedObjectModel(contentsOf: URL(fileURLWithPath: path)) else {
+            fatalError()
+        }
+
+        persistentStore = NSPersistentContainer(name: "GifModel", managedObjectModel: model)
+        persistentStore.loadPersistentStores { (_, _) in
+
+        }
+    }
+}
+
