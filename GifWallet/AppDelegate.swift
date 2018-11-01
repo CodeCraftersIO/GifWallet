01191908bc35a96d390a45f849361c783273c511
Pierluigi Cifani <pcifani@theleftbit.com>
2018-11-01 11:37:17 +0100

    Bootstrap is finished

---
+++ GifWallet/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  GifWallet
+//
+//  Created by Pierluigi Cifani on 02/03/2018.
+//  Copyright © 2018 Pierluigi Cifani. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        guard NSClassFromString("XCTest") == nil else {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            self.window?.rootViewController = UIViewController()
+            self.window?.makeKeyAndVisible()
+            return true
+        }
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UIViewController()
+        self.window?.makeKeyAndVisible()
+
+        return true
+    }
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+
+platform :ios, '10.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'GifWallet' do
+  pod 'SDWebImage'
+end
+
+target 'GifWalletKit' do
+    pod 'Alamofire'
+end
