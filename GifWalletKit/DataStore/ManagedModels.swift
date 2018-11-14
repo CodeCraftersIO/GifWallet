CodeCraftersIO/GifWallet_April2018

Add code to create and fetch Tags

GifWalletKit/DataStore/ManagedModels.swift
@@ -1,0 +1,16 @@
+//
+//  Created by Pierluigi Cifani on 29/03/2018.
+//  Copyright © 2018 Code Crafters. All rights reserved.
+//
+
+import CoreData
+
+extension ManagedGIF {
+    var tags: Set<String> {
+        guard let managedTags = self.managedTags as? Set<ManagedTag> else {
+            return []
+        }
+        return Set(managedTags.flatMap { return $0.name })
+    }
+}
+

CodeCraftersIO/GifWallet

GifWalletKit/DataStore/ManagedModels.swift
@@ -1,0 +1,16 @@
+//
+//  Created by Pierluigi Cifani on 29/03/2018.
+//  Copyright © 2018 Code Crafters. All rights reserved.
+//
+
+import CoreData
+
+extension ManagedGIF {
+    var tags: Set<String> {
+        guard let managedTags = self.managedTags as? Set<ManagedTag> else {
+            return []
+        }
+        return Set(managedTags.compactMap { return $0.name })
+    }
+}
+